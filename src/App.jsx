bc0eb4829eb401141a14746c310d953f8b1312bb
Dhee Chowdhury <dhee.chowdhury@gmail.com>
2024-05-29 11:36:01 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import FormWithController from "./components/FormWithController/FormWithController";
+
+function App() {
+  return (
+    <div>
+      <FormWithController />
+    </div>
+  );
+}
+
+export default App;
